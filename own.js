00920ea1045279dac941334be76beed5c9e3a4ed
Nasirmomin <145658887+Nasirmomin@users.noreply.github.com>
2024-06-06 11:55:08 +0530

    Add files via upload

---
+++ own.js
@@ -0,0 +1,10 @@
+// Prevent dropdown menu from closing when click inside the form
+ $(document).on("click", ".navbar-right .dropdown-menu", function(e){
+         e.stopPropagation();
+});
+
+
+
+  
+
+ 
